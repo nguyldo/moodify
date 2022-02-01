3b2290eeecd27a9a2812e3ca9f7fc1c46cc126fd
nguyldo <nguyldo@gmail.com>
2022-01-31 21:57:55 -0500

    first commit, client and server frameworks set up

---
+++ moodify-server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello Moodify World');
+});
+
+app.listen(port, () => {
+  console.log(`\nMoodify server listening on port ${port}`);
+});
