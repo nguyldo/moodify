af0ddcb5915a503cdc1dea0a07720ddd0f6ca953
nguyldo <nguyldo@gmail.com>
2022-03-29 01:44:29 -0400

    added genres to artists

--- moodify-server/functions/spotifyArtist.js
+++ moodify-server/functions/spotifyArtist.js
@@ -1,3 +1,4 @@
+/* eslint-disable guard-for-in */
 const axios = require('axios');
 
 const spotifyUrl = 'https://api.spotify.com/v1';
@@ -8,7 +9,20 @@ async function getArtistGenres(ids, token) {
     params: { ids },
   });
 
-  console.log('here');
+  const filteredArtists = data.data.artists.map((artist) => ({
+    name: artist.name,
+    id: artist.id,
+    url: artist.external_urls.spotify,
+    genres: artist.genres,
+  }));
+
+  const ret = {};
+
+  for (const artist of filteredArtists) {
+    ret[artist.id] = artist;
+  }
+
+  return ret;
 }
 
 module.exports = { getArtistGenres };
